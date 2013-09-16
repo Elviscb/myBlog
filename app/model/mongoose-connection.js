79c7b9f8c1902977735b0c4b3b622aed224ce1e8
cob <cb050034@gmail.com>
2013-09-16 14:14:11 +0800

    fix error

--- app/model/mongoose-connection.js
+++ app/model/mongoose-connection.js
@@ -8,7 +8,7 @@
 var mongoose = require('mongoose-q')(require('mongoose'));
 
 mongoose.connect('mongodb://'+
-    process.env.MONGOLAB_URI || process.env.MONGOHQ_URI || '127.0.0.1:27017'+
+    (process.env.MONGOLAB_URI || process.env.MONGOHQ_URI || '127.0.0.1:27017')+
     '/myblog');
 
 var db = mongoose.connection;
